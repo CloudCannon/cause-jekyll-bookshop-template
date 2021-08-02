6b0dc26c6e513354b15db9c953b383951f9d22bf
Jordan Trbuhovic <jordan@cloudcannon.com>
2021-08-03 11:31:23 +1200

    Bookshop settings

--- _cloudcannon-prebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-GEM_LOC=$(bundle info --path cc-template-components) && ln -sfn $GEM_LOC bookshop__gem
-npm install
-
-[ "$JEKYLL_ENV" != 'production' ] && npm run bookshop-live
-
-echo "📚 ✅ : Prebuild script finished"
